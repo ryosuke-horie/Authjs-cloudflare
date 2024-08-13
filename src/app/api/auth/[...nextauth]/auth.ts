078eca4a5c7fd66510787f7ff2a6720eac52247a
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-08-13 21:07:13 +0900

    drizzle

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -1,9 +1,12 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
- 
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import db from "../../../../../db";
+
 export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
+  adapter: DrizzleAdapter(db),
   providers: [Google],
 });
